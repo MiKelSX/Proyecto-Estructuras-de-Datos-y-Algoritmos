4917ad4d95a248b9a8ba3158a029eeef727f412b
MiKelSX <139016051+MiKelSX@users.noreply.github.com>
2025-01-10 22:39:46 -0300

    Delete v1.0/fuente directory

--- v1.0/fuente/src/busqueda_fuerza_bruta.py
+++
@@ -1,6 +0,0 @@
-def busqueda_fuerza_bruta(texto, patron):
-    resultados = []
-    for i, linea in enumerate(texto):
-        if patron in linea:
-            resultados.append((i, linea.strip()))
-    return resultados
